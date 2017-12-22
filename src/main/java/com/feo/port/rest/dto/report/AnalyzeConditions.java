2eb1fd6f826d1e60369f6c2de88c49868357b124
duanwj <duanwj6563@gmail.com>
2017-12-22 18:33:23 +0800

    修改配置

--- src/main/java/com/feo/port/rest/dto/report/AnalyzeConditions.java
+++ src/main/java/com/feo/port/rest/dto/report/AnalyzeConditions.java
@@ -1,5 +1,8 @@
 package com.feo.port.rest.dto.report;
 
+import lombok.Data;
+
+@Data
 public class AnalyzeConditions extends PageParameters {
     private String screen;//筛选条件
     private String sort;//排序条件
@@ -8,62 +11,6 @@ public class AnalyzeConditions extends PageParameters {
     private String startTime;//开始时间
     private String endTime;//结束时间
     private String legion;//军团
-
-    public String getScreen() {
-        return screen;
-    }
-
-    public void setScreen(String screen) {
-        this.screen = screen;
-    }
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getRange() {
-        return range;
-    }
-
-    public void setRange(String range) {
-        this.range = range;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getLegion() {
-        return legion;
-    }
-
-    public void setLegion(String legion) {
-        this.legion = legion;
-    }
 
     public AnalyzeConditions() {
     }
